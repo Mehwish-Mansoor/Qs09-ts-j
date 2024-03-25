be0cf1765b49bb791a499d9924b5205c82f7e181
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-25 22:56:35 +0500

    Delete qno6.ts

--- qno6.ts
+++
@@ -1,6 +0,0 @@
-// Stripping Name:store a person's name, and include some whitespace chracters at the beginning  and end of the name. Make sure you use each 
-// character combination,"/t" and "/n", at least once. print the name once.so the whitespace around the name is display. Then print the name after striping the whitespace.
-
-console.log("HelloMyWorld")
-console.log("Hello\tMy\tWorld")
-console.log("Hello\nMy\nWorld")
