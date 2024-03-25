95cbe189e7afaad7030169eaad43dc00ff8b6943
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-25 23:00:35 +0500

    Add files via upload

---
+++ qno9.js
@@ -0,0 +1,6 @@
+"use strict";
+//  9. Favorite Number: Store your favorite number in a variable.
+// Then, using that variable,creat a message that reveals your  
+// favorite number. Print that message.
+let num = 12345;
+console.log(`My favourite number is ${num}`);
---
+++ qno9.ts
@@ -0,0 +1,7 @@
+//  9. Favorite Number: Store your favorite number in a variable.
+// Then, using that variable,creat a message that reveals your  
+// favorite number. Print that message.
+
+let num:number = 12345
+console.log(`My favourite number is ${num}`);
+
